73702eca86ea6c6776f1ee5a0dfdf7cad5215418
dngst <dngst@protonmail.com>
2023-01-26 15:49:33 +0300

    Annotate

--- app/models/line_item_date.rb
+++ app/models/line_item_date.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: line_item_dates
+#
+#  id         :bigint           not null, primary key
+#  quote_id   :bigint           not null
+#  date       :date             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class LineItemDate < ApplicationRecord
   belongs_to :quote
 
